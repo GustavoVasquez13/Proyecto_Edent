f2ad17380e29cc56b29bec815273e9451a392cb1
marvin <marvin@marvin-PC>
2018-04-10 17:03:58 -0600

    Marvin nuevo login

--- src/formularios/f_recuperarc.java
+++ src/formularios/f_recuperarc.java
@@ -11,7 +11,7 @@ import Clases.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.ResultSet; 
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
