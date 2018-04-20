9c930848be7186d2ca2c14503a68e4c46c4dd4ea
ricar <ricar@DESKTOP-O1UUVPI.mshome.net>
2018-04-20 15:32:33 -0600

    Ricardo Martinez: se agregaron fondos formulario de ortodoncia

--- src/formularios/ExpedienteOrtodoncia.java
+++ src/formularios/ExpedienteOrtodoncia.java
@@ -6,15 +6,17 @@
 package formularios;
 
 import Clases.InsertarDatosBD;
+import Clases.internalFrameImagen;
 
 
 
 
-public class ExpedienteOrtodoncia extends javax.swing.JInternalFrame {
+public class ExpedienteOrtodoncia extends internalFrameImagen {
 
 
     public ExpedienteOrtodoncia() {
         initComponents(); 
+        setImagenw("img2.jpg");
     }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
