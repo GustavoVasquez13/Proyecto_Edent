090379b63114d396b288b711654e04bf3d49020b
marvin <marvin@marvin-PC>
2018-04-22 22:56:59 -0600

    Marvin;fondo igual a los demas formularios

--- src/formularios/frmLogin.java
+++ src/formularios/frmLogin.java
@@ -35,7 +35,7 @@ funciones v=new funciones();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        panel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/fondoL.png"))); // NOI18N
+        panel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Clases/img2.jpg"))); // NOI18N
         panel2.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
             public void mouseDragged(java.awt.event.MouseEvent evt) {
                 panel2MouseDragged(evt);
