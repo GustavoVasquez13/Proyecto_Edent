befe78e2fb2a8e8e84ba7fe4897a233a9994d3bb
mauba <mauba@MB>
2018-05-18 15:34:48 -0600

    Mauricio: agrego la opcion de editar productos

--- src/formularios/frmProducto.java
+++ src/formularios/frmProducto.java
@@ -1,13 +1,21 @@
 package formularios;
 
+import Clases.ActualizarDatosBD;
 import Clases.ConsultarDatosBD;
 import Clases.internalFrameImagen;
 import Clases.InsertarDatosBD;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JOptionPane;
+import javax.swing.RowFilter;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 
 public class frmProducto extends internalFrameImagen {
-
+    
+    int codigoProd;
+    private TableRowSorter trsFiltrarProd;
+    
     public frmProducto() {
         initComponents();
         setImagenw("img2.jpg"); 
@@ -27,6 +35,9 @@ public class frmProducto extends internalFrameImagen {
         btnCerrar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jtProd = new javax.swing.JTable();
+        btnEditar = new javax.swing.JButton();
+        txtBuscarProd = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
 
         setClosable(true);
         setIconifiable(true);
@@ -54,6 +65,7 @@ public class frmProducto extends internalFrameImagen {
         btnCerrar.setForeground(new java.awt.Color(0, 102, 204));
         btnCerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/cancelar.png"))); // NOI18N
         btnCerrar.setText("Cerrar");
+        btnCerrar.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         btnCerrar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         btnCerrar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         btnCerrar.addActionListener(new java.awt.event.ActionListener() {
@@ -72,44 +84,81 @@ public class frmProducto extends internalFrameImagen {
             }
         ));
         jtProd.getTableHeader().setReorderingAllowed(false);
+        jtProd.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jtProdMousePressed(evt);
+            }
+        });
         jScrollPane1.setViewportView(jtProd);
+
+        btnEditar.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        btnEditar.setForeground(new java.awt.Color(0, 102, 204));
+        btnEditar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/editar.png"))); // NOI18N
+        btnEditar.setText("Editar");
+        btnEditar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnEditar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        btnEditar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditarActionPerformed(evt);
+            }
+        });
+
+        txtBuscarProd.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtBuscarProdKeyTyped(evt);
+            }
+        });
+
+        jLabel3.setText("Buscar producto: ");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(48, 48, 48)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnGuardar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 63, Short.MAX_VALUE)
+                        .addComponent(btnEditar)
+                        .addGap(63, 63, 63)
+                        .addComponent(btnCerrar))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(jLabel2)
                             .addComponent(jLabel4))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 299, Short.MAX_VALUE)
-                            .addComponent(txtNombreP, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txtDescripcion))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtDescripcion)
+                            .addComponent(txtNombreP))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 52, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnGuardar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnCerrar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel3)
+                        .addGap(18, 18, 18)
+                        .addComponent(txtBuscarProd, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(53, 53, 53))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(335, 335, 335)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGap(19, 19, 19)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(50, 50, 50)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(150, 150, 150)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtBuscarProd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))
+                        .addGap(35, 35, 35)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(78, 78, 78)
+                    .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2)
                             .addComponent(txtNombreP, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -117,11 +166,12 @@ public class frmProducto extends internalFrameImagen {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel4)
                             .addComponent(txtDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(61, 61, 61)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(btnGuardar)
-                            .addComponent(btnCerrar, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(89, Short.MAX_VALUE))
+                        .addGap(98, 98, 98)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(btnGuardar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnCerrar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnEditar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(30, Short.MAX_VALUE))
         );
 
         pack();
@@ -130,6 +180,7 @@ public class frmProducto extends internalFrameImagen {
     public void limpiarTxt(){
         txtNombreP.setText("");
         txtDescripcion.setText("");
+        txtBuscarProd.setText("");
     }
     //muestra los productos agregados
     public void mostrarProd() {
@@ -138,6 +189,7 @@ public class frmProducto extends internalFrameImagen {
             ConsultarDatosBD funcion = new ConsultarDatosBD();
             modelo = funcion.mostrarProductos();
             jtProd.setModel(modelo);
+            this.btnEditar.setEnabled(false);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Problema al Consultar los Datos de Producto");
         }
@@ -159,14 +211,64 @@ public class frmProducto extends internalFrameImagen {
             JOptionPane.showMessageDialog(null, "Debe Ingresar El nombre del producto y su descripcion");
         }        
     }//GEN-LAST:event_btnGuardarActionPerformed
+
+    private void btnEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditarActionPerformed
+        if(txtNombreP.getText().length() !=0 && txtDescripcion.getText().length()!=0){
+            String nombre = txtNombreP.getText().toUpperCase();
+            String descripcion = txtDescripcion.getText().toUpperCase();
+            ActualizarDatosBD editar = new ActualizarDatosBD();
+            editar.ActualizarProd(nombre, descripcion, codigoProd);
+            limpiarTxt();
+            mostrarProd();
+            this.btnEditar.setEnabled(false);
+            this.btnGuardar.setEnabled(true);
+            
+        }else{
+            JOptionPane.showMessageDialog(null, "Debe Ingresar El nombre del producto y su descripcion");
+        }      
+        
+    }//GEN-LAST:event_btnEditarActionPerformed
+
+    private void jtProdMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jtProdMousePressed
+        int filaSelect = jtProd.getSelectedRow();
+        String codigoProdu = (String)jtProd.getValueAt(filaSelect, 0);
+        codigoProd = Integer.parseInt(codigoProdu);
+        String nombreProd = (String)jtProd.getValueAt(filaSelect, 1);
+        txtNombreP.setText(nombreProd);
+        String desc = (String)jtProd.getValueAt(filaSelect,2);
+        txtDescripcion.setText(desc);
+        this.btnGuardar.setEnabled(false);
+        this.btnEditar.setEnabled(true);
+        txtBuscarProd.setText("");
+    }//GEN-LAST:event_jtProdMousePressed
+
+    private void txtBuscarProdKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtBuscarProdKeyTyped
+        
+        txtBuscarProd.addKeyListener(new KeyAdapter()
+        {
+            public void keyReleased(final KeyEvent e){
+                String cadenaProv = (txtBuscarProd.getText().toUpperCase());
+                txtBuscarProd.setText(cadenaProv);
+                repaint();
+                trsFiltrarProd.setRowFilter(RowFilter.regexFilter(txtBuscarProd.getText(),1));
+                jtProd.getSelectionModel().setSelectionInterval(0,1);
+            }
+        });
+        trsFiltrarProd = new TableRowSorter(jtProd.getModel());
+        jtProd.setRowSorter(trsFiltrarProd);
+    }//GEN-LAST:event_txtBuscarProdKeyTyped
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCerrar;
+    private javax.swing.JButton btnEditar;
     private javax.swing.JButton btnGuardar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jtProd;
+    private javax.swing.JTextField txtBuscarProd;
     private javax.swing.JTextField txtDescripcion;
     private javax.swing.JTextField txtNombreP;
     // End of variables declaration//GEN-END:variables
