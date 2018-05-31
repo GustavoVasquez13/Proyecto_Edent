0fd4569bcd84356db2bdf345671930cf097a0e88
ricar <ricar@DESKTOP-O1UUVPI.mshome.net>
2018-05-31 16:09:42 -0600

    Ricardo Martinez: arreglo

--- src/Clases/ImagenFondo.java
+++ src/Clases/ImagenFondo.java
@@ -14,7 +14,7 @@ public class ImagenFondo implements Border{
  
     public ImagenFondo(){
         try {
-            URL imagePath = new URL(getClass().getResource("../Clases/img2.jpg").toString());
+            URL imagePath = new URL(getClass().getResource("../Clases/inicio.png").toString());
             back = ImageIO.read(imagePath);
         } catch (Exception ex) {            
         }
